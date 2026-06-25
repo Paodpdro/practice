415d6640e435089f6d91b7fa84960bd80a1d9c35
Paodpdro <126562990+Paodpdro@users.noreply.github.com>
2026-06-26 00:29:12 +0300

    These are the short tasks or exercises during the events subsection in the DOM Manipulation lesson.

---
+++ domEvents.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+    alert("Hello World");
+});
+
+
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+}
+
+const btn3 = document.querySelector("#btn3");
+btn3.onclick = alertFunction;
+
+const btn4 = document.querySelector("#btn4");
+btn4.addEventListener("click", alertFunction);
+
+const btn5 = document.querySelector("#btn5");
+btn5.addEventListener("click", function(e) {
+    e.target.style.background = "blue";
+})
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        alert(button.id);
+    });
+});
